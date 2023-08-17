636f54f1387eece9e7965c64f30c4e3653474ec8
Faith Fechser <faithfechser@gmail.com>
2023-08-17 14:18:41 -0600

    fix footer by putting it into div

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -2,14 +2,16 @@ import React from 'react'
 
 const Footer = () => {
   return (
-    <ul className="footer">
-        <li>
-            <a href="https://www.linkedin.com/in/faith-fechser/" alt="LinkedIn">LinkedIn</a>
-        </li>
-        <li>
-            <a href="https://github.com/faithfechser" alt="GitHub">GitHub</a>
-        </li>
-    </ul>
+    <div className="footer">
+      <ul>
+          <li>
+              <a href="https://www.linkedin.com/in/faith-fechser/" alt="LinkedIn">LinkedIn</a>
+          </li>
+          <li>
+              <a href="https://github.com/faithfechser" alt="GitHub">GitHub</a>
+          </li>
+      </ul>
+    </div>
   )
 }
 
