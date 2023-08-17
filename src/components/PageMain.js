c1f938292db5713bf35576f455ff3813206bd70d
Faith Fechser <faithfechser@gmail.com>
2023-08-17 14:17:21 -0600

    delete old code

--- src/components/PageMain.js
+++ src/components/PageMain.js
@@ -3,4 +3,3 @@ import AboutMe from "../pages/AboutMe";
 import ContactMe from "../pages/ContactMe";
 import Portfolio from "../pages/Portfolio";
 import Resume from "../pages/Resume";
-import { Header, NavBar, Footer } from "./PageBase"
